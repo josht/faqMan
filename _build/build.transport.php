16954bebcf9247a268996d1d6d98aaea798c8ea7
josht <josh@joshsmind.com>
2011-02-02 14:13:46 -0500

    New language means a version bump

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -34,8 +34,8 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','faqMan');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','1.0.2');
-define('PKG_RELEASE','beta1');
+define('PKG_VERSION','1.0.3');
+define('PKG_RELEASE','rc1');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
