9a9f8c79a4061c1e6a0cc28f77686c057c782c44
smooth-graphics <info@novolo.de>
2011-02-02 18:37:46 +0100

    added german translation
    Signed-off-by: smooth-graphics <info@novolo.de>

---
+++ core/components/faqman/lexicon/de/default.inc.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * faqMan
+ *
+ * Copyright 2010 by Josh Tambunga <josh+faqman@joshsmind.com>
+ *
+ * faqMan is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * faqMan is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * faqMan; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package faqman
+ */
+/**
+ * Default German Lexicon Entries for faqMan
+ * @translation by Anselm Hannemann (http://www.anselm-hannemann.de)
+ * @translation-date: 2011-02-02
+ * @package faqman
+ * @subpackage lexicon
+ */
+$_lang['faqman'] = 'FAQ Manager';
+$_lang['faqman.menu_desc'] = 'FAQ Listen einfach verwalten';
+$_lang['faqman.back_to_sets'] = 'zurück zu Listen';
+
+$_lang['faqman.set_intro_msg'] = 'FAQ-Listen verwalten.';
+$_lang['faqman.sets'] = 'Listen';
+$_lang['faqman.set_create'] = 'FAQ Liste erstellen';
+$_lang['faqman.set_err_ae'] = 'Eine FAQ Liste mit gleichem Namen existiert bereits.';
+$_lang['faqman.set_err_nf'] = 'FAQ Liste nicht gefunden.';
+$_lang['faqman.set_err_ns'] = 'keine FAQ Liste ausgewählt.';
+$_lang['faqman.set_err_remove'] = 'Es trat ein Fehler beim Löschen der FAQ Liste auf.';
+$_lang['faqman.set_err_save'] = 'Es trat ein Fehler beim Speichern der FAQ Liste auf.';
+$_lang['faqman.set_remove'] = 'FAQ Liste entfernen';
+$_lang['faqman.set_remove_confirm'] = 'Sind Sie sicher, dass Sie die FAQ Liste entfernen möchten? (sämtliche FAQ Einträge werden ebenfalls gelöscht)';
+$_lang['faqman.set_update'] = 'FAQ Liste bearbeiten';
+$_lang['faqman.set_manage'] = 'FAQ Liste verwalten';
+
+$_lang['faqman.item_intro_msg'] = 'FAQ Einträge verwalten.';
+$_lang['faqman.items'] = 'Einträge';
+$_lang['faqman.item_create'] = 'Eintrag erstellen';
+$_lang['faqman.item_err_ae'] = 'Ein FAQ Eintrag mit gleichem Namen existiert bereits.';
+$_lang['faqman.item_err_nf'] = 'Eintrag nicht gefunden.';
+$_lang['faqman.item_err_ns'] = 'kein Eintrag ausgewählt.';
+$_lang['faqman.item_err_remove'] = 'Es trat ein Fehler beim Löschen des FAQ Eintrags auf.';
+$_lang['faqman.item_err_save'] = 'Es trat ein Fehler beim Speichern des FAQ Eintrags auf.';
+$_lang['faqman.item_remove'] = 'Eintrag entfernen';
+$_lang['faqman.item_remove_confirm'] = 'Sind Sie sicher, dass Sie den FAQ Eintrag entfernen möchten?';
+$_lang['faqman.item_update'] = 'Eintrag bearbeiten';
+
+$_lang['faqman.intro_msg'] = 'Einträge verwalten.';
+
+$_lang['faqman.question'] = 'Frage';
+$_lang['faqman.answer'] = 'Antwort';
---
+++ core/components/faqman/lexicon/de/properties.inc.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * faqMan
+ *
+ * Copyright 2010 by Josh Tambunga <josh+faqman@joshsmind.com>
+ *
+ * faqMan is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * faqMan is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * faqMan; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package faqman
+ */
+/**
+ * Properties German Lexicon Entries for faqMan
+ * @translation by Anselm Hannemann (http://www.anselm-hannemann.de)
+ * @translation-date: 2011-02-02
+ * @package faqman
+ * @subpackage lexicon
+ */
+$_lang['prop_faqman.limit_desc'] = 'Anzahl an FAQ-Einträgen pro Seite.';
+$_lang['prop_faqman.outputseparator_desc'] = 'String, um die Zeilen zu trennen.';
+$_lang['prop_faqman.sortby_desc'] = 'Sortierung nach Feld.';
+$_lang['prop_faqman.sortdir_desc'] = 'Sortierungsrichtung.';
+$_lang['prop_faqman.tpl_desc'] = 'Chunk für jeden Eintrag.';
+$_lang['prop_faqman.toplaceholder_desc'] = 'Falls gesetzt, wird der Inhalt in den hier angegebenen Platzhalter ausgegeben.';
