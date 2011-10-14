5377cfd54ede91aa26e36f55eacb9b8a2c80bc8f
Mark Hamstra <hello@markhamstra.com>
2011-10-14 23:44:27 +0200

    Add Dutch translation

---
+++ core/components/faqman/lexicon/nl/default.inc.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * faqMan
+ *
+ * Copyright 2010 by Josh Tambunga <josh+faqman@joshsmind.com>
+ *
+ * faqMan is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * faqMan is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * faqMan; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package faqman
+ */
+/**
+ * Default English Lexicon Entries for faqMan
+ *
+ * @package faqman
+ * @subpackage lexicon
+ */
+$_lang['faqman'] = 'FAQ Beheer';
+$_lang['faqman.menu_desc'] = 'Simpel FAQ beheer.';
+$_lang['faqman.back_to_sets'] = 'Terug naar Sets';
+
+$_lang['faqman.set_intro_msg'] = 'Beheer FAQ sets.';
+$_lang['faqman.sets'] = 'Sets';
+$_lang['faqman.set_create'] = 'Maak nieuwe FAQ Set';
+$_lang['faqman.set_err_ae'] = 'Een FAQ set bestaat al met die naam.';
+$_lang['faqman.set_err_nf'] = 'FAQ set niet gevonden.';
+$_lang['faqman.set_err_ns'] = 'FAQ set niet gespecificeerd.';
+$_lang['faqman.set_err_remove'] = 'Er is een fout opgetreden tijdens het verwijderen van de FAQ set.';
+$_lang['faqman.set_err_save'] = 'Er is een fout opgetreden tijdens het opslaan van de FAQ set.';
+$_lang['faqman.set_remove'] = 'Verwijder FAQ set';
+$_lang['faqman.set_remove_confirm'] = 'Weet je zeker dat je deze FAQ set wilt verwijderen? (Alle vragen en antwoorden worden ook verwijderd)';
+$_lang['faqman.set_update'] = 'Bewerk FAQ set';
+$_lang['faqman.set_manage'] = 'Beheer FAQ set';
+
+$_lang['faqman.item_intro_msg'] = 'Beheer je veelgestelde vragen.';
+$_lang['faqman.items'] = 'Vragen';
+$_lang['faqman.item_create'] = 'Nieuwe Vraag';
+$_lang['faqman.item_err_ae'] = 'Er bestaat al een vraag met die naam.';
+$_lang['faqman.item_err_nf'] = 'Vraag niet gevonden.';
+$_lang['faqman.item_err_ns'] = 'Vraag niet gespecificeerd.';
+$_lang['faqman.item_err_remove'] = 'Er is een fout opgetreden tijdens het verwijderen van de vraag.';
+$_lang['faqman.item_err_save'] = 'Er is een fout opgetreden tijdens het opslaan van de vraag.';
+$_lang['faqman.item_remove'] = 'Verwijder Vraag';
+$_lang['faqman.item_remove_confirm'] = 'Weet je zeker dat je deze vraag wilt verwijderen?';
+$_lang['faqman.item_update'] = 'Bewerk Vraag';
+
+$_lang['faqman.intro_msg'] = 'Beheer je veelgestelde vragen.';
+
+$_lang['faqman.question'] = 'Vraag';
+$_lang['faqman.answer'] = 'Antwoord';
+$_lang['faqman.toggle_answers'] = 'Antwoorden Tonen/Verbergen';
---
+++ core/components/faqman/lexicon/nl/properties.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * faqMan
+ *
+ * Copyright 2010 by Josh Tambunga <josh+faqman@joshsmind.com>
+ *
+ * faqMan is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * faqMan is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * faqMan; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package faqman
+ */
+/**
+ * Properties English Lexicon Entries for faqMan
+ *
+ * @package faqman
+ * @subpackage lexicon
+ */
+$_lang['prop_faqman.limit_desc'] = 'Het aantal FAQs om weer te geven per pagina.';
+$_lang['prop_faqman.outputseparator_desc'] = 'Scheidingstekens tussen elke row.';
+$_lang['prop_faqman.sortby_desc'] = 'Het veld om op te sorteren.';
+$_lang['prop_faqman.sortdir_desc'] = 'Richting om op te sorteren.';
+$_lang['prop_faqman.tpl_desc'] = 'Chunk om te gebruiken voor elke vraag.';
+$_lang['prop_faqman.toplaceholder_desc'] = 'Wanner ingevuld zal het resultaat in de placeholder gezet worden die je aangeeft in plaats van direct weer te geven.';
