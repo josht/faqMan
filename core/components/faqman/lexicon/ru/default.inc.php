cd8666220df1585f58fe9168d484c102e54c4087
Dmitry Kozlovich <dmitry.kozlovich@itstartuplabs.com>
2014-10-15 19:48:29 +0300

    Add russian translations

---
+++ core/components/faqman/lexicon/ru/default.inc.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * faqMan
+ *
+ * Copyright 2010 by Josh Tambunga <josh+faqman@joshsmind.com>
+ *
+ * faqMan is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * faqMan is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * faqMan; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package faqman
+ */
+/**
+ * Default Russian Lexicon Entries for faqMan
+ *
+ * @package faqman
+ * @subpackage lexicon
+ */
+$_lang['faqman'] = 'Менеджер FAQ';
+$_lang['faqman.menu_desc'] = 'Простое управление FAQ';
+$_lang['faqman.back_to_sets'] = 'Назад к группам';
+
+$_lang['faqman.set_intro_msg'] = 'Управление группами FAQ.';
+$_lang['faqman.sets'] = 'Группы';
+$_lang['faqman.set_create'] = 'Создать группу FAQ';
+$_lang['faqman.set_err_ae'] = 'Группа FAQ с таким именем уже существует.';
+$_lang['faqman.set_err_nf'] = 'Группа FAQ не найдена.';
+$_lang['faqman.set_err_ns'] = 'Группа FAQ не задана.';
+$_lang['faqman.set_err_remove'] = 'При удалении группы FAQ возникла ошибка.';
+$_lang['faqman.set_err_save'] = 'При сохранении группы FAQ возникла ошибка.';
+$_lang['faqman.set_remove'] = 'Удалить группу FAQ';
+$_lang['faqman.set_remove_confirm'] = 'Вы уверены, что хотите удалить эту группу FAQ? (Все связанные вопросы и ответы будут удалены)';
+$_lang['faqman.set_update'] = 'Обновить группу FAQ';
+$_lang['faqman.set_manage'] = 'Управлять группой FAQ';
+
+$_lang['faqman.item_intro_msg'] = 'Управление вопросами и ответами.';
+$_lang['faqman.items'] = 'Элементы';
+$_lang['faqman.item_create'] = 'Создать элемент';
+$_lang['faqman.item_err_ae'] = 'Элемент с таким именем уже существует.';
+$_lang['faqman.item_err_nf'] = 'Элемент не найден.';
+$_lang['faqman.item_err_ns'] = 'Элемент не задан.';
+$_lang['faqman.item_err_remove'] = 'При удалении элемента возникла ошибка.';
+$_lang['faqman.item_err_save'] = 'При сохранении элемента возникла ошибка.';
+$_lang['faqman.item_remove'] = 'Удалить элемент';
+$_lang['faqman.item_remove_confirm'] = 'Вы уверены, что хотите удалить этот элемент?';
+$_lang['faqman.item_update'] = 'Обновить элемент';
+
+$_lang['faqman.intro_msg'] = 'Управление элементами.';
+
+$_lang['faqman.question'] = 'Вопрос';
+$_lang['faqman.answer'] = 'Ответ';
+$_lang['faqman.toggle_answers'] = 'Вкл/выкл предпросмотр ответов';
+
+$_lang['setting_faqman.use_richtext'] = 'Использовать визуальный редактор';
