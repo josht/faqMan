950738872c8419b4eda320025b36f20c9a830032
Josh Tambunga <josh@joshsmind.com>
2014-11-12 19:17:52 -0600

    Fixed error on faqManItem creation
    Fixes #27

--- core/components/faqman/processors/mgr/item/create.php
+++ core/components/faqman/processors/mgr/item/create.php
@@ -21,30 +21,24 @@
  */
 /**
  * Create an Item
- * 
+ *
  * @package faqman
  * @subpackage processors
  */
-if (empty($scriptProperties['set'])) return $modx->error->failure($modx->lexicon('faqman.set_err_ns'));
-$alreadyExists = $modx->getObject('faqManItem',array(
-    'name' => $_POST['name'],
-));
-if ($alreadyExists) {
-    $modx->error->addField('name',$modx->lexicon('faqman.item_err_ae'));
-}
 
-if ($modx->error->hasError()) {
-    return $modx->error->failure();
-}
+// Make sure we are submitting something to create
+if (empty($_POST['set'])) return $modx->error->failure($modx->lexicon('faqman.set_err_ns'));
 
+// Create a new faqManItem object and fill it with information
 $item = $modx->newObject('faqManItem');
 $item->fromArray($_POST);
 
+// New faqManItems are added at the end of the list
 $total = $modx->getCount('faqManItem',array('set' => $_POST['set']));
 $item->set('rank',$total);
 
+// Try to save and return our status
 if ($item->save() == false) {
     return $modx->error->failure($modx->lexicon('faqman.item_err_save'));
 }
-
 return $modx->error->success('',$item);
